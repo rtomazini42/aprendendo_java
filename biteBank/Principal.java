a1923d09122cd036c2c834cfe088adb5e66f11ce
Renan Tomazini <renantomazini@gmail.com>
2018-10-24 19:45:50 -0300

    Update Principal.java

--- biteBank/Principal.java
+++ biteBank/Principal.java
@@ -7,7 +7,10 @@ public class Principal {
 		conta1.setSaldo(200);
 		conta1.setSaldo(conta1.getSaldo() + 100);
 		System.out.println(conta1.getSaldo());
-
+		conta1.setNome("Yuri");
+		String user;
+		user = conta1.getNome();
+		System.out.println(user);
 	}
 
 }
