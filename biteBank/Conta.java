1e45cffc992c1372ae2c8d21dad4cdc5747ddd8e
Renan Tomazini <renantomazini@gmail.com>
2018-10-24 19:41:35 -0300

    Create Conta.java

---
+++ biteBank/Conta.java
@@ -0,0 +1,33 @@
+package bityBank;
+
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private String nome;
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+}
